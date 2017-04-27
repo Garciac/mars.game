964c333cd7f050723ce1f65027f420c29789c076
Garciac <chrisgarcia@Chriss-MacBook-Air.local>
2017-04-26 20:25:14 -0700

    spawn position

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -10,7 +10,7 @@ game.PlayScreen = me.ScreenObject.extend({
 
         me.levelDirector.loadLevel("marslevel01");
 
-        this.resetPlayer();
+        this.resetPlayer(0, 400);
 
         me.input.bindKey(me.input.KEY.RIGHT, "right");
 
@@ -27,8 +27,8 @@ game.PlayScreen = me.ScreenObject.extend({
         me.game.world.removeChild(this.HUD);
     },
 
-    resetPlayer: function () {
-        var player = me.pool.pull("mario", 0, 410, {});
+    resetPlayer: function (x, y) {
+        var player = me.pool.pull("mario", x, y, {});
         me.game.world.addChild(player, 3);
     }
 
