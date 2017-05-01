3d2881e9ac641526739586eb09efef116524ae3a
Garciac <chrisgarcia@Chriss-MacBook-Air.local>
2017-04-30 22:03:54 -0700

    Character can now move left, right, and jump

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -22,27 +22,61 @@ game.PlayerEntity = me.Entity.extend({
         this.body.setVelocity(5, 20);
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
-
-    update: function (delta) {
-        if (me.input.isKeyPressed("right")) {
+    
+    update: function(delta){
+        
+        //Check if the right button is pressed
+        if(me.input.isKeyPressed("left")) {
+            
+            //This says to unflip the image if it is flipped.
+            this.flipX(true);
+            //Adds the spped set in the setVeelocity method above to our current position and multiplies be timer.tick to.
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        } 
+        else if(me.input.isKeyPressed("right")) {
+            
+            //Flips the image
+            this.flipX(false);
             this.body.vel.x += this.body.accel.x * me.timer.tick;
-
-        } else {
+        }
+        else {
             this.body.vel.x = 0;
         }
-
+        
+        if(me.input.isKeyPressed("jump")) {
+           if (!this.body.jumping && !this.body.falling) {
+                this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+                this.body.jumping = true;
+           }
+        }
+        
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
-
-        if (this.body.vel.x !== 0) {
-            if (!this.renderable.isCurrentAnimation("smallWalk")) {
-                this.renderable.setCurrentAnimation("smallWalk");
-                this.renderable.setAnimationFrame();
+        
+        if(!this.big){
+            if(this.body.vel.x !== 0) {
+                if(!this.renderable.isCurrentAnimation("smallWalk")) {
+                    this.renderable.setCurrentAnimation("smallWalk");
+                    this.renderable.setAnimationFrame();
+                }
             }
-        } else {
+            else{
+                this.renderable.setCurrentAnimation("idle");
+            }
+        }
+        else{
+            if(this.body.vel.x !== 0) {
+                if(!this.renderable.isCurrentAnimation("smallWalk")) {
+                    this.renderable.setCurrentAnimation("smallWalk");
+                    this.renderable.setAnimationFrame();
+                }
+            }
+                else{
             this.renderable.setCurrentAnimation("idle");
         }
-
+    }
+        
+        
         this._super(me.Entity, "update", [delta]);
         return true;
     },
@@ -70,23 +104,23 @@ game.LevelTrigger = me.Entity.extend({
 
 });
 
-game.BadGuy = me.Entity.extend({
-    init: function (x, y, settings) {
-        this._super(me.Entity, 'init', [x, y, {
-                image: "asteroid",
-                spritewidth: "312",
-                spriteheight: "336",
-                width: 312,
-                height: 336,
-                getShape: function () {
-                    return (new me.Rect(0, 0, 312, 336)).toPolygon();
-                }
-            }]);
+//game.BadGuy = me.Entity.extend({
+  //  init: function (x, y, settings) {
+    //    this._super(me.Entity, 'init', [x, y, {
+      //          image: "asteroid",
+        //        spritewidth: "312",
+          //      spriteheight: "336",
+            //    width: 312,
+              //  height: 336,
+                //getShape: function () {
+                  //  return (new me.Rect(0, 0, 312, 336)).toPolygon();
+             //   }
+           // }]);
 
-    },
+   // },
 
-    update: function (delta) {
+   // update: function (delta) {
 
-    }
-});
+   // }
+//});
 
